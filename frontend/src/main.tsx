6ad0cc711adf60e6f44f6c5fb8f0e2655fa6e7ae
MWolff89 <150514371+MWolff89@users.noreply.github.com>
2024-03-20 11:51:48 +0000

    document.cookie always to tesas default cookie

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,9 @@ import { v4 as uuidv4 } from "uuid";
 import App from "./App.tsx";
 import "./index.css";
 
-if (document.cookie.indexOf("user_id") === -1) {
-  document.cookie = `opengpts_user_id=${uuidv4()}`;
-}
+// if (document.cookie.indexOf("user_id") === -1) {
+  // document.cookie = `opengpts_user_id=${uuidv4()}`;
+document.cookie = `opengpts_user_id=1f51d6ef-5336-4b8a-82e8-6d452d54a197`;
+// }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
